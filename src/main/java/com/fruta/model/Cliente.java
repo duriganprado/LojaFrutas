917d61dfe8190ce242830c793d1c5780a16a4ab0
kosnika25 <karenvaliaodeabreu@gmail.com>
2025-11-28 21:45:35 -0300

    wip

--- src/main/java/com/fruta/model/Cliente.java
+++ src/main/java/com/fruta/model/Cliente.java
@@ -23,6 +23,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+
 @Entity
 @Table(name = "clientes")
 public class Cliente {
